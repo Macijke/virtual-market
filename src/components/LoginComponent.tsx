ec7d67c7a076fcfab4550f6bc075c0f372bfe400
Maciej "Macijke" Hosaniak <maciekhosaniak@o2.pl>
2024-11-12 17:37:33 +0100

    Add Login

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,23 +1,54 @@
+import {useState} from 'react';
+import {useNavigate} from "react-router-dom";
 
 function Login() {
-    function onsubmit(e) {
-        e.preventDefault();
-        const login = e.target.login.value;
-        const password = e.target.password.value;
-        console.log(login, password);
-    }
+
+    const history = useNavigate();
+    const [error, setError] = useState<string | null>(null);
+    const [loginData, setLoginData] = useState({
+        login: '',
+        password: ''
+    });
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginData({
+            ...loginData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const login = async (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log(loginData);
+        fetch(`http://localhost:3003/login`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(loginData),
+        }).then(response => response.json()).then(json => {
+            console.log(json)
+            if (json.token) {
+                localStorage.setItem('token', json.token);
+                history('/');
+            } else {
+                setError('Nieprawidłowy adres email lub hasło. Spróbuj ponownie.');
+            }
+        }).catch(console.error);
+    };
 
     return (
         <article className="bg-gray-700 h-screen flex items-center justify-center">
         <div className="w-full max-w-xs">
-            <form onSubmit={onsubmit} className="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-500">
+            <form onSubmit={login} className="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-500">
                 <div className="mb-4">
                     <label className="block text-white font-bold mb-2 text-xl" htmlFor="login">
                         Login
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="login" name="login" type="text" placeholder="Login"/>
+                        id="login" name="login" type="text" placeholder="Login" onChange={handleInputChange}/>
                 </div>
                 <div className="mb-6">
                     <label className="block text-white text-xl font-bold mb-2" htmlFor="password">
@@ -25,9 +56,10 @@ function Login() {
                     </label>
                     <input
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" type="password" name="password" placeholder="******************"/>
+                        id="password" type="password" name="password" placeholder="******************" onChange={handleInputChange}/>
                     {/*<p className="text-red-500 text-xs italic">Wprowadź hasło.</p>*/}
                 </div>
+                {error && <div className="alert alert-danger">{error}</div>}
                 <div className="flex items-center justify-between">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Login from "./components/LoginComponent";
+import Login from "./components/LoginComponent.tsx";
 import NotFound from "./components/NotFoundComponent";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
